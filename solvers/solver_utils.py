412c03c17e54620e6ccc72175eb50c70dc50017a
kostejnv <v.kostejn@seznam.cz>
2022-03-22 22:07:08 +0100

    add algorithm power 2

--- solvers/solver_utils.py
+++ solvers/solver_utils.py
@@ -1,8 +1,14 @@
 from entity.category import Category
 from entity.event import Event
+from math import ceil,log2
 
 def set_all_intervals_same(cats):
     max_interval = max([cat.min_interval for cat in cats.values()])
     for cat in cats.values():
         cat.min_interval = max_interval
     return max_interval, cats
+
+def set_all_intervals_to_power_2(cats):
+    for cat in cats.values():
+        cat.min_interval = 2**(ceil(log2(cat.min_interval)))
+    return cats
